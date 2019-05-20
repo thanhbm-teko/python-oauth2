9be8886c2857f57fabeb5b9ab5ca41d452766eda
Thanh Bui Minh <thanhbuiminh@Thanhs-MacBook-Pro.local>
2019-05-20 11:10:38 +0700

    trying to send coverage to code beat

---
+++ codebeat.sh
@@ -0,0 +1,3 @@
+export CODECLIMATE_API_HOST=https://codebeat.co/webhooks/code_coverage
+export CODECLIMATE_REPO_TOKEN=8b28a954-d88f-4b6b-a6c5-72b8c859071d
+codeclimate-test-reporter --file .coverage --debug
